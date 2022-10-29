e39b0e15078d6a4249070b8d50f8b1cdc9917632
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-10-29 21:57:34 +0530

    Using Dequeue to remove numbers in Queue

---
+++ QueueDequeue.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections;
+
+namespace QueueUsingLinkedlist
+{
+    class StactAndQueue
+    {
+        static public void Main()
+        {
+
+
+            Queue numbers = new Queue();
+
+
+            numbers.Enqueue(56);
+            numbers.Enqueue(30);
+            numbers.Enqueue(70);
+
+            Console.WriteLine("Total elements present in queue: " + numbers.Count);
+
+            numbers.Dequeue();
+
+            
+            Console.WriteLine("Total elements present in queue: " + numbers.Count);
+
+
+
+            //numbers.Clear();
+
+            
+            //Console.WriteLine("Total elements present in queue: " + numbers.Count);
+
+
+
+
+            foreach (var ele in numbers)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+
+}
