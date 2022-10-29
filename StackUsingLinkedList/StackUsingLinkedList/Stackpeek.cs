5598c652909059907c6c1e5e5b48d3d9a813062a
Sangmeshwar96 <patil.sangmeshwar4u@gmail.com>
2022-10-29 18:29:00 +0530

    Using Peep Command in Stack

---
+++ StackUsingLinkedList/StackUsingLinkedList/Stackpeek.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackUsingLinkedList
+{
+    class StactAndQueue
+    {
+
+
+        static public void Main()
+        {
+
+            Stack numbers = new Stack();
+
+
+            numbers.Push(70);
+            numbers.Push(30);
+            numbers.Push(56);
+
+            Console.WriteLine("Number of elements in the Stack: " + numbers.Count);
+
+            Console.Write("Top element of Stack is: ");
+
+            Console.WriteLine(numbers.Peek());
+
+
+
+            //Console.Write(numbers.Pop());
+
+
+            Console.WriteLine("\nNumber of elements in the Stack: " + numbers.Count);
+
+
+
+            foreach (var elem in numbers)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
