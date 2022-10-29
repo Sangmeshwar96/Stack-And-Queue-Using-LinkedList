3a066c85cc73fcf3d56f4ea14dc557a932c0e8c0
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-10-29 21:44:29 +0530

    Using Enqueue to add numbers in Queue

---
+++ Queue.cs
@@ -0,0 +1,29 @@
+﻿// C# program to illustrate queue
+using System;
+using System.Collections;
+
+namespace QueueUsingLinkedlist
+{
+    class StactAndQueue
+    {
+        static public void Main()
+        {
+
+
+            Queue numbers = new Queue();
+
+
+            numbers.Enqueue(56);
+            numbers.Enqueue(30);
+            numbers.Enqueue(70);
+
+
+
+            foreach (var ele in numbers)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+
+}
