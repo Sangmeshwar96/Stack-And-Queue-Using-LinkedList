6e5f7dcf524eede0e8d15f653ad11e0dd33c22de
Sangmeshwar96 <patil.sangmeshwar4u@gmail.com>
2022-10-29 17:34:39 +0530

    Pushing 3 Numbers using LinkedList

---
+++ StackUsingLinkedList/StackUsingLinkedList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace StackUsingLinkedList
+{
+    class StactAndQueue
+    {
+
+
+        static public void Main()
+        {
+
+            Stack numbers = new Stack();
+
+
+            numbers.Push(70);
+            numbers.Push(30);
+            numbers.Push(56);
+           
+
+            //data.Pop();
+
+
+            foreach (var elem in numbers)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
